df593cf7022f5900677cc1ef49e3ec9f151ef795
damdam6 <dambi626626@gmail.com>
2023-09-03 23:56:38 +0900

    feat: [#-]Controller 코드 파일 생성
    page1 Controller 페이지 생성

--- flutter_pagebuild/lib/Controller_Main.dart
+++ flutter_pagebuild/lib/Controller_Main.dart
@@ -1,1 +1,11 @@
+import 'package:get/get.dart';
 
+//controller 변화 체크용 코드
+class Controller_Main extends GetxController {
+  final count = 0.obs;
+
+  void increment() {
+    count.value++;
+    // count(count.value + 1);
+  }
+}
