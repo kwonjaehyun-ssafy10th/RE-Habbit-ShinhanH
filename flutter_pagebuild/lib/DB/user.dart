4240157935cc3b3f252f239cc5acd1b970f9a7f0
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-09-10 19:40:06 +0900

    feat: [#-] user.dart 업데이트
    json으로 encode하기 쉽게 인스턴스를 Map으로 변환하는 toMap 메서드 추가

--- flutter_pagebuild/lib/DB/user.dart
+++ flutter_pagebuild/lib/DB/user.dart
@@ -14,17 +14,27 @@ class User {
         (random.nextInt(100000) + 100000).toString(),
         "입출금계좌",
         "자유로운SOL통장",
-        100000));
+        100000).toMap());
     accountList.add(Account(this.name,
-        (random.nextInt(100000) + 100000).toString(), "자유적금", "나만의SOL적금", 0));
+        (random.nextInt(100000) + 100000).toString(), "자유적금", "나만의SOL적금", 0).toMap());
   }
 
   toString() {
     return this.name + this.accountList.toString();
   }
+
+  Map<dynamic, dynamic> toMap() {
+    Map<dynamic, dynamic> map = new Map();
+    map['고객명'] = this.name;
+    for (int i = 0; i < this.accountList.length; i++) {
+      map[i] = this.accountList[i];
+    }
+    return map;
+  }
 }
 
 void main() {
   var user = User('김신한');
+  print(user.toMap());
   print(user);
 }
