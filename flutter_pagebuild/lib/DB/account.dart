25df228d3fa9f82f6533d0fc8254cf4067ccf14c
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-09-10 19:35:16 +0900

    feat: [#-] account.dart 업데이트
    json으로 encode하기 쉽게 인스턴스를 Map으로 변환하는 toMap 메서드 추가

--- flutter_pagebuild/lib/DB/account.dart
+++ flutter_pagebuild/lib/DB/account.dart
@@ -21,9 +21,19 @@ class Account {
         this.balance.toString() +
         "]";
   }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = new Map();
+    map['고객명'] = this.name;
+    map['계좌번호'] = this.accountNo;
+    map['구분'] = this.type;
+    map['상품명'] = this.title;
+    return map;
+  }
 }
 
 void main() {
   Account a1 = new Account("김신한", "123", "적금", "내적금", 300000);
   print(a1);
+  print(a1.toMap());
 }
