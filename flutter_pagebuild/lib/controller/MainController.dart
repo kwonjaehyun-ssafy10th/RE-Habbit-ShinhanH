ef1f6ec006d66f992d78c73bd750d4420b9cc2e0
damdam6 <dambi626626@gmail.com>
2023-09-10 20:00:14 +0900

    feat: [#28-]MainController 구현
    일부 구현 -Userclass 생성

--- flutter_pagebuild/lib/controller/MainController.dart
+++ flutter_pagebuild/lib/controller/MainController.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_pagebuild/controller/RegisController.dart';
 import 'package:flutter_pagebuild/view/DetailView.dart';
 import 'package:flutter_pagebuild/view/MainView.dart';
@@ -15,16 +16,28 @@ import 'package:flutter_pagebuild/controller/DetailController.dart';
 import 'package:flutter_pagebuild/controller/RankController.dart';
 import 'package:flutter_pagebuild/controller/StampController.dart';
 
-class MainController extends GetxController {
-  void getController() {
-    Get.lazyPut<DetailController>(() => DetailController());
-    Get.lazyPut<RankController>(() => RankController());
-    Get.lazyPut<StampController>(() => StampController());
+class resetMainModel with ChangeNotifier {
+  //싱글턴
+  resetMainModel.privateConstructor();
+  static final resetMainModel _inst = resetMainModel.privateConstructor();
+  static resetMainModel get inst => _inst;
+
+//MainController에서 바꿔야 할 내용
+//1. 사용자 명
+//2. 챌린지 명
+//3. 그래프 % 실시간 계산
+
+//사용자 정보 저장 -> 1회만 설정하기
+  void setUser(String username, String challengeName, String chkAccount,
+      String savings) {
+    MainModel.inst.user.username = username;
+    MainModel.inst.user.challengeName = challengeName;
+    MainModel.inst.user.chkAccount = chkAccount;
+    MainModel.inst.user.savings = savings;
   }
 
-//장면 보여주는 뷰
-  void showMain() {
-    Get.to(const MainView());
+  User getUser() {
+    return MainModel.inst.user;
   }
 
 //원형 그래프
@@ -45,17 +58,30 @@ class MainController extends GetxController {
     int rate = ((MainModel.inst.sucRate) * 100).toInt();
     return rate;
   }
+}
+
+class MainController extends GetxController {
+  void getController() {
+    Get.lazyPut<DetailController>(() => DetailController());
+    Get.lazyPut<RankController>(() => RankController());
+    Get.lazyPut<StampController>(() => StampController());
+  }
+
+//장면 보여주는 뷰
+  void showMain() {
+    Get.to(const MainView());
+  }
 
 //하단부 -  컨트롤러들
   void goToDetail() {
-    Get.to(DetailView());
+    Get.to(const DetailView());
   }
 
   void goToRank() {
-    Get.to(RankView());
+    Get.to(const RankView());
   }
 
   void goToStamp() {
-    Get.to(StampView());
+    Get.to(const StampView());
   }
 }
