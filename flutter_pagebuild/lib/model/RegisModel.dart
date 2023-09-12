87aabb5c5ef158f104c0375484d4add75358d159
damdam6 <dambi626626@gmail.com>
2023-09-12 16:12:51 +0900

    feat: [#-]RegisModel

--- flutter_pagebuild/lib/model/RegisModel.dart
+++ flutter_pagebuild/lib/model/RegisModel.dart
@@ -3,8 +3,16 @@ import 'package:flutter_pagebuild/model/MainModel.dart';
 import 'package:get/get.dart';
 
 class RegisModel {
-  //싱글턴
+  //싱글턴 패턴 선언
   RegisModel.privateConstructor();
   static final RegisModel _inst = RegisModel.privateConstructor();
   static RegisModel get inst => _inst;
+
+  String registName = '';
+  String challengeName = '';
+
+  int challenge = 0;
+  int savingAmount = 0;
+
+  List<Map<Map<String, String>, int>> accountList = [];
 }
