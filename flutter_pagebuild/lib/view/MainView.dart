a9c5d877fb7073b88600b689422dc4c15a6496e1
HYESEUNG <h103906@naver.com>
2023-09-12 14:51:27 +0900

    feat: 메인페이지 디자인 수정
    여백 및 그래프, 컨테이너 수정
    
    Related to: #41

--- flutter_pagebuild/lib/view/MainView.dart
+++ flutter_pagebuild/lib/view/MainView.dart
@@ -23,6 +23,7 @@ class _MainViewState extends State<MainView> {
       title: 'Flutter Chart',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: '아리따-돋움',
       ),
       home: const MyHomePage(title: 'Flutter Chart'),
     );
@@ -43,7 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    double blankHeight = screenHeight * 0.05;
+    double blankHeight = screenHeight * 0.02;
+    double blankWidth = screenWidth * 0.05;
     double startHeight = screenHeight * 0.1;
     double contentWidth = screenWidth * 0.8;
 
@@ -84,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       onPressed: () {
                         controller.goToRank();
                       },
-                      child: const Text(
+                      child: Text(
                         '랭킹보기',
                         style: TextStyle(
                           color: Colors.black,
@@ -93,8 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         backgroundColor: MaterialStateProperty.all<Color?>(Color.fromARGB(255, 246, 255, 207)),
                       ),
                     ),
-                    const SizedBox(
-                      width: 30,
+                    SizedBox(
+                      width: blankWidth,
                     ),
                     ElevatedButton(
                       onPressed: () {
@@ -112,88 +114,109 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ),
               ),
+
               SizedBox(
                 height: blankHeight,
               ),
-              // 달성률 현황
-              Text(
-                '현재 달성률 🏃‍♀️',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-                
-              ),
-              
-              Text(
-                // '챌린지성공 ${succRate} % \n적금성공 ${moneyRate} % \n',
-                '챌린지성공 80 % + 적금 10 %',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                  // fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: blankHeight,
-              ),
-              // 달성률 차트
-              PieChart(
-                chartType: ChartType.ring,
-                dataMap: dataMap,
-                animationDuration: Duration(milliseconds: 800),
-                colorList: colorList,
-                chartLegendSpacing: 50,
-                chartRadius: contentWidth/3,
-                initialAngleInDegree: 0,
-                ringStrokeWidth: 60,
 
-                
-                // 범례
-                legendOptions: const LegendOptions(
-
-                  showLegendsInRow: false,
-                  legendPosition: LegendPosition.right,
-                  showLegends: true,
-                
-                ),
-                chartValuesOptions: const ChartValuesOptions(
-                  showChartValueBackground: false,
-                  showChartValues: false,
-                  showChartValuesInPercentage: true,
-                  showChartValuesOutside: false,
-                  decimalPlaces: 1,
-                  chartValueStyle: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold
-                  )
-                ),
-              ),
-
-              // 현재 진행중인 챌린지 및 이미지
+              // 달성률 관련 위젯들 묶음
               Stack(
-                children: <Widget>[
-                  const Positioned(
-                    top: 100,
-                    left: 150,
-                    child: Text(
-                      '커피 사먹지 않기 진행 중...',
-                      // '${challenge_name} 진행중'
-                      style: TextStyle(
-                        fontSize: 20,
+                
+                children: [
+                  Container(
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 234, 244, 255),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 209, 209, 209).withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 7), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    
+                    width: screenWidth,
+                    height: screenHeight*0.35,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: blankHeight,
+                        ),
+                        // 달성률 현황
+                        const Text(
+                          '현재 달성률 🏃‍♀️',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w300,
+                            color: Color.fromARGB(255, 0, 113, 206),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        
+                        // !! 성공률 변수 넣기 !!
+                        const Text(
+                          // '챌린지성공 ${succRate} % \n적금성공 ${moneyRate} % \n',
+                          '챌린지성공 80 % + 적금 10 %',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 130,
+                    left: 60,
+                    child: PieChart(
+                      // 달성률 차트
+                      chartType: ChartType.ring,
+                      dataMap: dataMap,
+                      animationDuration: Duration(milliseconds: 800),
+                      colorList: colorList,
+                      chartLegendSpacing: 50,
+                      chartRadius: contentWidth/3,
+                      initialAngleInDegree: 0,
+                      ringStrokeWidth: 60,
+
+                      
+                      // 범례
+                      legendOptions: const LegendOptions(
+
+                        showLegendsInRow: false,
+                        legendPosition: LegendPosition.right,
+                        showLegends: true,
+                      
+                      ),
+                      chartValuesOptions: const ChartValuesOptions(
+                        showChartValueBackground: false,
+                        showChartValues: false,
+                        showChartValuesInPercentage: true,
+                        showChartValuesOutside: false,
+                        decimalPlaces: 1,
+                        chartValueStyle: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold
+                        )
                       ),
                     ),
                   ),
-                  Image.asset(
-                    'assets/images/main-image.png',
-                  ),
-                ],
-              ),
+                
+              ],
+            ),
+              
+              
 
+              // 현재 진행중인 챌린지 및 이미지
+              Image.asset(
+                'assets/images/main-image.png',
+              ),
+                
             ],
           ),
         ),
@@ -202,6 +225,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+// !! 비율 데이터 여기에 넣으면 된다 !!
 // 파이차트 데이터 
 Map<String, double> dataMap = {
     "챌린지성공": 80.0,
@@ -219,6 +243,7 @@ final colorList = <Color>[
 
 
 // 헤더위젯
+// 안 띄우는 게 나을수도
 class HeaderWidget extends StatelessWidget {
   const HeaderWidget({super.key});
 
@@ -237,12 +262,27 @@ class UserWidget extends StatelessWidget {
     return const Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(
-          '시계토끼 님',
-          // '${user_name} 님'
-          style: TextStyle(
-            fontSize: 28,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              '시계토끼 ',
+              // '${user_name}'
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              '님',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+
+        ],
         ),
       ],
     );
