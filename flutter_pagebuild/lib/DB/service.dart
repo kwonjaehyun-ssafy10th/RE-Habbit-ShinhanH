ef4a0f1bb7d7c431c8e0a0665ea12ebb6e6becbd
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-09-16 08:02:19 +0900

    feat: [#-] service.dart 업데이트
    - 데이터맵의 구조를 보여주는 dataMap 추가
    - 데이터맵에 필요한 데이터 추가하여 dataToMap 메서드 업데이트

--- flutter_pagebuild/lib/DB/service.dart
+++ flutter_pagebuild/lib/DB/service.dart
@@ -1,9 +1,24 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'light_account.dart';
 
 String domain = 'shb-hackton-ad177-default-rtdb.firebaseio.com';
 
-//사용자 정보 가져오는 리스트
+Map dataMap = {
+  'user': {
+    '_user': {
+      'account': ['_accountNo1', '_accountNo2'],
+      'lastChecked': '_time',
+      'stamp': {
+        'day': '_day',
+        'stampCnt': ['_succeedCnt', '_failCnt', '_moneyCnt'],
+        'stampList': ['_day1', '_day2', '_day3', '_day4', '_day5', '_day6', '_day7', '_day8', '_day9', '_day10', '_day11', '_day12', '_day13', '_day14', '_day15', '_day16', '_day17', '_day18', '_day19', '_day20', '_day21', '_day22', '_day23', '_day24', '_day25', '_day26', '_day27', '_day28', '_day29', '_day30'],
+        '고객명': '_user'
+      }
+    }
+  }
+};
+
 Future getDataMapOf(user) async {
   var path = 'service/user/$user';
   final url = Uri.https(domain, "$path.json");
@@ -11,9 +26,16 @@ Future getDataMapOf(user) async {
   return json.decode(response.body);
 }
 
-//사용자 정보 업로드 하는 함수
-Map<String, dynamic> dataToMap(
-    String name, String accountNo1, String accountNo2) {
+void patchUserData(user, accountNo1, accountNo2) async {
+  var path = 'service/user/$user';
+  final url = Uri.https(domain, "$path.json");
+  await http.patch(
+  url,
+  body: json.encode(dataToMap(user, accountNo1, accountNo2)),
+  );
+}
+
+Map<String, dynamic> dataToMap(String name, String accountNo1, String accountNo2) {
   Map<String, dynamic> map = {};
   map['고객명'] = name;
   Map<String, dynamic> account = {};
@@ -25,21 +47,14 @@ Map<String, dynamic> dataToMap(
   stamp['day'] = 0;
   List stampCnt = [0, 0, 0];
   stamp['stampCnt'] = stampCnt;
+  stamp['stampList'] = List<int>.filled(30, 0);
   map['stmap'] = stamp;
   return map;
 }
 
-void patchUserData(user, accountNo1, accountNo2) async {
-  var path = 'service/user/$user';
-  final url = Uri.https(domain, "$path.json");
-  await http.patch(
-    url,
-    body: json.encode(dataToMap(user, accountNo1, accountNo2)),
-  );
-}
 
 void main() async {
-  print(await getDataMapOf("도레미"));
-  print(await getDataMapOf("최쏠"));
-  patchUserData("박리노", "100301", "302581");
+  // print(await getDataMapOf("도레미"));
+  // print(await getDataMapOf("최쏠"));
+  patchUserData("최몰리", createAccountNo(), createAccountNo());
 }
