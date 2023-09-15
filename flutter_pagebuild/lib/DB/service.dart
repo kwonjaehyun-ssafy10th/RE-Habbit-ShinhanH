dad21a6fe1818d278c750832a0d51171225e1dcf
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-09-15 22:42:38 +0900

    feat: [#-] service.dart 업데이트
    고객명, 계좌번호1, 계좌번호2 받아서 service DB에 올리는 기능 추가

--- flutter_pagebuild/lib/DB/service.dart
+++ flutter_pagebuild/lib/DB/service.dart
@@ -10,7 +10,33 @@ Future getDataMapOf(user) async {
   return json.decode(response.body);
 }
 
+Map<String, dynamic> dataToMap(String name, String accountNo1, String accountNo2) {
+  Map<String, dynamic> map = {};
+  map['고객명'] = name;
+  Map<String, dynamic> account = {};
+  account['0'] = accountNo1;
+  account['1'] = accountNo2;
+  map['account'] = account;
+  map['lastChecked'] = 0;
+  Map<String, dynamic> stamp = {};
+  stamp['day'] = 0;
+  List stampCnt = [0, 0, 0];
+  stamp['stampCnt'] = stampCnt;
+  map['stmap'] = stamp;
+  return map;
+}
+
+void patchUserData(user, accountNo1, accountNo2) async {
+  var path = 'service/user/$user';
+  final url = Uri.https(domain, "$path.json");
+  await http.patch(
+  url,
+  body: json.encode(dataToMap(user, accountNo1, accountNo2)),
+  );
+}
+
 void main() async {
   print(await getDataMapOf("도레미"));
   print(await getDataMapOf("최쏠"));
+  patchUserData("박리노", "100301", "302581");
 }
