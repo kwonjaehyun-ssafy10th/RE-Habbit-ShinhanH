55213806c514494b988aa6c6aa43fc73b66c55cd
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-09-15 10:30:24 +0900

    feat: [#-] light_transfer.dart 업데이트
    이체 시 거래상대가 DB에 없는 경우 처리 내용 추가

--- flutter_pagebuild/lib/DB/light_transfer.dart
+++ flutter_pagebuild/lib/DB/light_transfer.dart
@@ -12,7 +12,11 @@ transfer(date, time, from, to, amount, memoFrom, memoTo, type) async {
   var balance = await getBalanceOf(from);
   if (balance >= amount) {
     patchTransaction(from, date, time, type, amount, 0, memoFrom, to);
-    patchTransaction(to, date, time, type, 0, amount, memoTo, from);
+    if (await getNameOf(to) != null) {
+      patchTransaction(to, date, time, type, 0, amount, memoTo, from);
+    } else {
+      patchDiffBankAccount(to, memoTo);
+    }
   } else {
     print("출금가능금액이 부족합니다!");
   }
