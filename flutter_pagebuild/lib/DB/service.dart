7f6a22ba870ae975c2e17f80bd69cf6b95894ca3
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-09-16 09:38:01 +0900

    feat: [#-] service.dart 업데이트
    - dataToMap 메서드 오타 수정(stmap -> stamp)
    - user data 업데이트하는 메서드 추가
        - updateLastChecked
        - updateDay
        - updateStampListDayOf
        - updateStampCnt

--- flutter_pagebuild/lib/DB/service.dart
+++ flutter_pagebuild/lib/DB/service.dart
@@ -1,19 +1,18 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'light_account.dart';
 
 String domain = 'shb-hackton-ad177-default-rtdb.firebaseio.com';
 
 Map dataMap = {
   'user': {
-    '_user': {
+    '_user1': {
       'account': ['_accountNo1', '_accountNo2'],
-      'lastChecked': '_time',
+      'lastChecked': '-time',
       'stamp': {
         'day': '_day',
         'stampCnt': ['_succeedCnt', '_failCnt', '_moneyCnt'],
         'stampList': ['_day1', '_day2', '_day3', '_day4', '_day5', '_day6', '_day7', '_day8', '_day9', '_day10', '_day11', '_day12', '_day13', '_day14', '_day15', '_day16', '_day17', '_day18', '_day19', '_day20', '_day21', '_day22', '_day23', '_day24', '_day25', '_day26', '_day27', '_day28', '_day29', '_day30'],
-        '고객명': '_user'
+        '고객명': '_user1'
       }
     }
   }
@@ -48,13 +47,64 @@ Map<String, dynamic> dataToMap(String name, String accountNo1, String accountNo2
   List stampCnt = [0, 0, 0];
   stamp['stampCnt'] = stampCnt;
   stamp['stampList'] = List<int>.filled(30, 0);
-  map['stmap'] = stamp;
+  map['stamp'] = stamp;
   return map;
+}
+
+updateLastChecked(String user, int time) async {
+  var path = 'service/user/$user';
+  final url = Uri.https(domain, "$path.json");
+  Map<String, dynamic> map = {};
+  map["lastChecked"] = time;
+  await http.patch(
+  url,
+  body: json.encode(map),
+  );
+}
+
+updateDay(String user, int day) async {
+  var path = 'service/user/$user/stamp';
+  final url = Uri.https(domain, "$path.json");
+  Map<String, dynamic> map = {};
+  map["day"] = day;
+  await http.patch(
+  url,
+  body: json.encode(map),
+  );
+}
+
+updateStampListDayOf(String user, int day, int stamp) async {
+  var path = 'service/user/$user/stamp/stampList';
+  final url = Uri.https(domain, "$path.json");
+  Map<String, dynamic> map = {};
+  map[(day - 1).toString()] = stamp;
+  await http.patch(
+  url,
+  body: json.encode(map),
+  );
+  updateStampCnt(user, day, stamp);
+}
+
+updateStampCnt(String user, int day, int stamp) async {
+  var path = 'service/user/$user/stamp/stampCnt';
+  final url = Uri.https(domain, "$path.json");
+  var response = await http.get(url);
+  var cnt = json.decode(response.body)[stamp];
+  Map<String, dynamic> map = {};
+  map[stamp.toString()] = cnt + 1;
+  await http.patch(
+  url,
+  body: json.encode(map),
+  );
 }
 
 
 void main() async {
   // print(await getDataMapOf("도레미"));
   // print(await getDataMapOf("최쏠"));
-  patchUserData("최몰리", createAccountNo(), createAccountNo());
+  // patchUserData("박리노", "100301", "302581");
+  // updateLastChecked("도레미", 5);
+  // updateDay("도레미", 5);
+  // patchUserData("최몰리", "192102", "181923");
+  // updateStampListDayOf("도레미", 2, 1);
 }
